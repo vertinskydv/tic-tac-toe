3688947dbc4af734551a06e62dea07d3b6ed2706
Dmitry Vertinsky <vertinskydv@gmail.com>
2017-01-30 18:52:20 +0300

    a sketch of the solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,17 +1,40 @@
 class TicTacToe {
     constructor() {
-
+        this.previousMoveError = false;
+        this.prevSymbol = 'x';  //true = x; false = o
+        this.currentSymbol = 'o';
+        this.usedMoves = [];
     }
 
     getCurrentPlayerSymbol() {
-
+        if (!this.previousMoveError) {
+            [this.currentSymbol] = [this.prevSymbol];
+            this.previousMoveError = false;
+            return this.currentSymbol;
+        } else {
+            this.previousMoveError = false;
+            return this.currentSymbol;
+        };
     }
 
     nextTurn(rowIndex, columnIndex) {
+        let duplication = false;
+        this.usedMoves.forEach(function(currentCoor) {
+            if ((currentCoor[0] === rowIndex) && (currentCoor[1] === columnIndex)) {
+                duplication = true;
+            };
+            if (!duplicate) {
+                this.usedMovies.push([rowIndex, columnIndex]);
+            } else {
+                this.previousMoveError = true;
+            }
 
+
+        });
     }
 
     isFinished() {
+        if ()
 
     }
 
@@ -33,3 +56,21 @@ class TicTacToe {
 }
 
 module.exports = TicTacToe;
+
+let game;
+
+game = new TicTacToe();
+debugger;
+game.getCurrentPlayerSymbol();
+
+game.nextTurn(0, 1)
+game.getCurrentPlayerSymbol();
+
+game.nextTurn(1, 2)
+game.getCurrentPlayerSymbol();
+
+game.nextTurn(0, 2)
+game.getCurrentPlayerSymbol();
+
+game.nextTurn(0, 0)
+game.getCurrentPlayerSymbol();
